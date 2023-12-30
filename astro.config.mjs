a44b63d907ea15882cdac500e82eff7644f667c1
Ryan Miville <ryan.miville@terminus.com>
2023-12-29 21:17:50 -0500

    empty with shadcn/ui

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+import mdx from "@astrojs/mdx";
+
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), mdx(), react()],
+  output: "server",
+  adapter: vercel()
+});

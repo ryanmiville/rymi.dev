45118d59e7acf9025356dc5f9f238560effe9b55
Ryan Miville <ryan.miville@terminus.com>
2023-12-31 15:02:46 -0500

    Update default theme in ModeToggle component

--- src/components/ModeToggle.tsx
+++ src/components/ModeToggle.tsx
@@ -12,7 +12,7 @@ import {
 export function ModeToggle() {
   const [theme, setThemeState] = React.useState<
     "light" | "dark" | "system"
-  >("light")
+  >("system")
 
   React.useEffect(() => {
     const isDarkMode = document.documentElement.classList.contains("dark")

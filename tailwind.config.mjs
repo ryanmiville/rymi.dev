2921ff9ce76a8c1ad135c2c518ae9f3329916fae
Ryan Miville <ryan.miville@terminus.com>
2024-01-04 22:59:30 -0500

    transitions and font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,7 +18,7 @@ export default {
     },
     extend: {
 			fontFamily: {
-        sans: ["InterVariable", "Inter", ...defaultTheme.fontFamily.sans],
+        sans: ['"Inter Variable"', ...defaultTheme.fontFamily.sans],
       },
       colors: {
         border: "hsl(var(--border))",
